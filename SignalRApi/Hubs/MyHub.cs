3e888087b72ab77ed3cf051de01fabea850e598d
Eyyup Erdoğan <eyyup.erdogan@detaysoft.com>
2024-02-19 11:46:53 +0300

    Add project files.

---
+++ SignalRApi/Hubs/MyHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Hubs
+{
+    public class MyHub:Hub
+    {
+        public async Task SendMessage(string message)
+        {
+          await Clients.All.SendAsync("receiveMessage",message);
+        }
+    }
+}
---
+++ SignalR_Console_Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+class SignalR_Console_Client
+{
+    static async Task Main(string[] args)
+    {
+        //hub ile bağlantı işlemi yapılıyor
+        HubConnection connection = new HubConnectionBuilder()
+       .WithUrl("https://localhost:7178/myhub")
+       .Build();
+
+        await connection.StartAsync();
+
+        await Console.Out.WriteLineAsync($"ConnectionID:{connection.ConnectionId}");
+
+        //serverdan gelen mesaşları karşılama
+        connection.On<string>("receiveMessage", message =>
+        {
+             Console.WriteLine($"Message:{message}");
+        });
+
+        while (true)
+        {
+            if (Console.ReadKey().Key == ConsoleKey.M)
+            {
+                await Console.Out.WriteAsync("Lütfen göndereceğiniz mesajı yazınız.");
+                await Console.Out.WriteAsync("Mesaj :");
+                string message = Console.ReadLine();
+                await Console.Out.WriteLineAsync();
+                //SendMessage fonk. tetikler
+                await connection.InvokeAsync("SendMessage", message);
+            }
+        }
+    }
+}
+
